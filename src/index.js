27912d9c7167f106a79b764cb87a2fad1aa4fb3f
barakatimothy <timothybaraka39@gmail.com>
2023-06-20 11:23:12 -0400

    complete!

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+document.addEventListener("DOMContentLoaded",(e) => {
+   myTasks.addEventListener("submit", () =>
+   createNewTask(e,taskDescription)
+ );
+  let taskDescription = document.getElementById("new-task-description");
+
+  let myTasks = document.getElementById("create-task-form");
+ 
+  let TaskUl = document.getElementById("tasks");
 });
+  let createNewTask = (e, taskDescription) => {
+  
+   e.preventDefault();
+
+  let addNewTask = document.createElement("li");
+   addNewTask.innerText = taskDescription.value;
+
+  appendTask(addNewTask);
+};
+  let appendTask = task => {
+  document.getElementById("tasks").appendChild(task);
+};
